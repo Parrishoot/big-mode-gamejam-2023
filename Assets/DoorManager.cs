6ab0f34edf0e580f04840c8d7c6dfcb5bccda18e
Parrishoot <mparrish@u.rochester.edu>
2023-12-02 16:06:25 -0500

    Added neighbor logic

--- Assets/DoorManager.cs
+++ Assets/DoorManager.cs
@@ -4,24 +4,68 @@ using UnityEngine;
 
 public class DoorManager : ParameterizedEventIngester<RoomManager.RoomEvent>
 {
+    [SerializeField]
+    private LayerMask neighborLayerMask;
+
+    private DoorManager neighbor;
+
     protected override void Awake()
     {
         base.Awake();
-        gameObject.SetActive(false);
+        FindNeighbor();
     }
+
+    protected void Start() {
+        Close();
+    }
+
+    private void FindNeighbor() {
+        foreach (Collider collider in Physics.OverlapSphere(transform.position, 5f, neighborLayerMask, QueryTriggerInteraction.Collide))
+        {
+            if(collider.gameObject == this.gameObject) {
+                continue;
+            }
+
+            DoorManager neighborDoorManager = collider.gameObject.GetComponent<DoorManager>();
+
+            if(neighborDoorManager) {
+                neighbor = neighborDoorManager;
+            }
+        }
+    }
+
     protected override void OnEventTrigger(RoomManager.RoomEvent eventParams)
     {
-        Debug.Log("Ingesting!");
-
         switch(eventParams) {
 
             case RoomManager.RoomEvent.ROOM_ENTERED:
-                 gameObject.SetActive(true);
+                Close();
                 break;
 
             case RoomManager.RoomEvent.ROOM_COMPLETED:
-                 gameObject.SetActive(false);
+                Open();
                 break;
         }
+    }
+
+    private void Open() {
+        if(neighbor != null) {
+            gameObject.SetActive(false);
+            neighbor.gameObject.SetActive(false);
+        }
+    }
+
+    private void Close() {
+        
+        gameObject.SetActive(true);
+        
+        if(neighbor != null) {
+            neighbor.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnDrawGizmosSelected() {
+        Gizmos.color = new Color(0, 1, 0, .3f);
+        Gizmos.DrawSphere(transform.position, 5f);
     }
 }
